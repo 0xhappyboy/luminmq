5380978079d143c9c57758f920f722bf08b9661d
happyboy <superhappyboy1995@gmail.com>
2025-10-09 17:26:02 +0800

    update : modify the relevant macro logic.

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,89 +1,94 @@
-use std::collections::HashMap;
-
-use luminmq_core::types::ConsumerBinder;
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
-use syn::{FnArg, ItemFn, parse_macro_input};
+use syn::{
+    Ident, ItemFn, Lit, LitStr, Token,
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+};
 
-const REQUIRED_TYPE: &str = "Message";
 const REQUIRED_ATTR_KEY_1: &str = "group_id";
 const REQUIRED_ATTR_KEY_2: &str = "topic";
 
-#[proc_macro_attribute]
-pub fn consumer(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let fn_name = &input_fn.sig.ident;
-    let mut flag: bool = false;
-    // attribute list
-    let mut cache = HashMap::<String, String>::new();
-    let attrs_str = attr.to_string();
-    if !attrs_str.is_empty() {
-        let attrs: Vec<&str> = attrs_str.split(',').collect();
-        attrs.iter().for_each(|att| {
-            let v: Vec<&str> = att.split("=").collect();
-            if v.len() == 2 {
-                let k = v.get(0).unwrap().trim();
-                let v = v.get(1).unwrap().trim();
-                cache.insert(k.to_string(), v.to_string());
-            }
-        });
-    }
-    if cache.contains_key(REQUIRED_ATTR_KEY_1) && cache.contains_key(REQUIRED_ATTR_KEY_2) {
-        flag = true;
-    }
-    // list of function parameter types.
-    let mut arg_types = vec![];
-    for arg in &input_fn.sig.inputs {
-        if let FnArg::Typed(pat_type) = arg {
-            let ty = &pat_type.ty;
-            arg_types.push(ty);
-        }
-    }
-    let types: Vec<String> = arg_types.iter().map(|ty| quote!(#ty).to_string()).collect();
-    if types.len() == 1 {
-        types.iter().for_each(|t| {
-            if REQUIRED_TYPE.eq(t) {
-                flag = true;
-            } else {
-                flag = false;
-            }
-        });
-    }
-    if flag {
-        ConsumerBinder::insert(
-            (
-                cache.get(REQUIRED_ATTR_KEY_1).unwrap().to_string(),
-                cache.get(REQUIRED_ATTR_KEY_2).unwrap().to_string(),
-            ),
-            fn_name.to_string(),
-        );
-    }
-    let output = quote! {
-        #input_fn
-    };
-    output.into()
+struct ConsumerAttrs {
+    group_id: LitStr,
+    topic: LitStr,
 }
 
-#[proc_macro]
-pub fn exec_consume(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as syn::ExprTuple);
-    if input.elems.len() > 2 {
-        panic!("Expected a tuple with three elements: (function_name, arg1)");
-    }
-    let fn_name = input.elems[0].clone();
-    let arg1 = input.elems[1].clone();
-    let fn_name = if let syn::Expr::Lit(expr_lit) = &fn_name {
-        if let syn::Lit::Str(lit_str) = &expr_lit.lit {
-            lit_str
-        } else {
-            panic!("Function name must be a string literal");
+impl Parse for ConsumerAttrs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let group_id_ident: Ident = input.parse()?;
+        if group_id_ident != REQUIRED_ATTR_KEY_1 {
+            return Err(syn::Error::new(
+                group_id_ident.span(),
+                "Expected 'group_id'",
+            ));
         }
-    } else {
-        panic!("Function name must be a string literal");
+        input.parse::<Token![=]>()?;
+        let group_id_lit: Lit = input.parse()?;
+        let group_id = match group_id_lit {
+            Lit::Str(lit_str) => lit_str,
+            _ => {
+                return Err(syn::Error::new(
+                    group_id_lit.span(),
+                    "Expected string literal for 'group_id'",
+                ));
+            }
+        };
+        input.parse::<Token![,]>()?;
+        let topic_ident: Ident = input.parse()?;
+        if topic_ident != REQUIRED_ATTR_KEY_2 {
+            return Err(syn::Error::new(topic_ident.span(), "Expected 'topic'"));
+        }
+        input.parse::<Token![=]>()?;
+        let topic_lit: Lit = input.parse()?;
+        let topic = match topic_lit {
+            Lit::Str(lit_str) => lit_str,
+            _ => {
+                return Err(syn::Error::new(
+                    topic_lit.span(),
+                    "Expected string literal for 'topic'",
+                ));
+            }
+        };
+        Ok(ConsumerAttrs { group_id, topic })
+    }
+}
+
+/// a consumer function used to mark a message queue.
+/// The function signature must be in the following format:
+/// ```rust
+/// pub fn consumer(message: Message) -> Result<String, String>
+/// ```
+/// Example
+/// ```rust
+/// #[consumer(group_id = "group-test", topic = "topic-test")]
+/// fn say_bye(message: Message) -> Result<String, String> {
+///    // ......
+///  }
+/// ```
+#[proc_macro_attribute]
+pub fn consumer(attr: TokenStream, input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(attr as ConsumerAttrs);
+    let group_id_str = args.group_id.value();
+    let topic_str = args.topic.value();
+    if group_id_str.is_empty() {
+        panic!("group id cannot be empty")
+    }
+    if topic_str.is_empty() {
+        panic!("topic cannot be empty")
+    }
+    let input_fn = parse_macro_input!(input as ItemFn);
+    let fn_ident = &input_fn.sig.ident;
+    let fn_name_str = fn_ident.to_string();
+    let register_ident = Ident::new(&format!("register_{}", fn_name_str), Span::call_site());
+    let output = quote! {
+        #input_fn
+        #[ctor::ctor]
+        #[allow(non_snake_case)]
+        fn #register_ident() {
+             luminmq_core::types::ConsumerBinder::insert((#group_id_str.to_string(),#topic_str.to_string()),#fn_ident);
+        }
     };
-    let func_ident = syn::Ident::new(&fn_name.value(), fn_name.span());
-    let expanded = quote! {
-        #func_ident(#arg1)
-    };
-    TokenStream::from(expanded)
+    output.into()
 }
